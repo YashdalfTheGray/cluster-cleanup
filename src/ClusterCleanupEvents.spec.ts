0577d0b1a4f582b3f8b3727bba1bd8d043f74361
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-06-23 08:56:34 -0700

    added some tests for the event emitter

--- src/ClusterCleanupEvents.spec.ts
+++ src/ClusterCleanupEvents.spec.ts
@@ -1,7 +1,24 @@
 import test from 'ava';
+import { upperFirst } from 'lodash';
 
-const testFunction = async value => Promise.resolve(value);
+import { ClusterCleanupEventEmitter, ClusterCleanupEvents } from './ClusterCleanupEvents';
 
-test('passes', async (t) => {
-    t.is(await testFunction(true), true);
+const toMethodName = (event: string) => `on${upperFirst(event)}`;
+
+test('ClusterCleanupEventEmitter.removeAllListeners is fluent', (t) => {
+    const events = new ClusterCleanupEventEmitter();
+
+    t.not(events.removeAllListeners().emit, undefined);
+});
+
+Object.keys(ClusterCleanupEvents).map(k => toMethodName(k)).forEach((m) => {
+    const events = new ClusterCleanupEventEmitter();
+
+    test(`ClusterCleanupEventEmitter.${m} exists`, (t) => {
+        t.not(events[m], undefined);
+    });
+
+    test(`ClusterCleanupEventEmitter.${m} returns a listener remover`, (t) => {
+        t.is(typeof events[m](() => true), 'function');
+    });
 });

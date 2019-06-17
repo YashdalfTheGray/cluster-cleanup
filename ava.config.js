7c786af95dd0a2a6559e9dfb9d0cea636e78e9da
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-06-17 09:20:19 -0700

    added more node versions and updated ava config

--- ava.config.js
+++ ava.config.js
@@ -1,16 +1,7 @@
 export default {
-    files: [
-        'src/**/*.spec.ts'
-    ],
-    sources: [
-        'src/**/*.ts'
-    ],
-    compileEnhancements: false,
-    extensions: [
-        'ts',
-        'spec.ts'
-    ],
-    require: [
-        'ts-node/register'
-    ]
-}
+  files: ['src/**/*.spec.ts'],
+  sources: ['src/**/*.ts'],
+  compileEnhancements: false,
+  extensions: ['ts', 'spec.ts'],
+  require: ['ts-node/register']
+};

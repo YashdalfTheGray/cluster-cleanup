db89cf3fdaceb6080787bb53238bc3473553b45d
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-02-13 10:59:21 -0800

    moved more types into the common types file

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,8 @@
 import { ECSClientConfig } from '@aws-sdk/client-ecs';
 
+export type Listener<T> = (data: T) => void;
+export type RemoveListenerFunction = () => void;
+
 export interface ClusterCleanupConfig extends ECSClientConfig {
   enableFargate?: boolean;
 }

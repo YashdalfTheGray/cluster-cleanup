71afe0f3f1cf0af531a8f54714fb944b9a9cf5cc
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-02-14 21:16:47 -0800

    added the built assets to the dist folder

---
+++ dist/types.d.ts
@@ -0,0 +1,9 @@
+import { ECSClientConfig } from '@aws-sdk/client-ecs';
+export declare type Listener<T> = (data: T) => void;
+export declare type RemoveListenerFunction = () => void;
+export interface ClusterCleanupConfig extends ECSClientConfig {
+    enableFargate?: boolean;
+}
+export interface DeleteOptions {
+    verbose?: boolean;
+}

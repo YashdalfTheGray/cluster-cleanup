efd074f00046acacb3f7eb81127e8319cc58a3ba
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-02-18 08:39:32 -0800

    known failing test for now, while it gets built

--- test/functional/cleanup.spec.ts
+++ test/functional/cleanup.spec.ts
@@ -3,12 +3,30 @@ import { ECS } from '@aws-sdk/client-ecs';
 import test from 'ava';
 
 import { ClusterCleanup } from '../../src';
-import { MockAWSClient } from '../mocks';
+import { MockAwsClient } from '../mocks';
 
-test.skip('cleanup goes through all the motions', async (t) => {
+test.failing('cleanup goes through all the motions', async (t) => {
+  const seenCommands = [];
+  const mockAwsClient = new MockAwsClient((command) => {
+    seenCommands.push(command);
+  });
+
   const cleanup = new ClusterCleanup(
     { enableFargate: true },
-    {} as ECS,
-    {} as CloudFormation
+    mockAwsClient as unknown as ECS,
+    mockAwsClient as unknown as CloudFormation
   );
+
+  const cleanedUpResources = await cleanup.deleteClusterAndResources(
+    'active-test-cluster',
+    'test-stack',
+    {
+      verbose: true,
+      pollTimeoutMs: 1000,
+      pollIntervalMs: 100,
+    }
+  );
+
+  t.not(seenCommands.length, 0);
+  t.not(cleanedUpResources.length, 0);
 });

953faa96160f68c36b081717991667dc4dacdf59
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-03-02 09:18:27 -0800

    added a known cli options interface so we can keep track and flow types properly

--- src/types.ts
+++ src/types.ts
@@ -13,3 +13,17 @@ export interface DeleteOptions {
   waiterPollMinDelayMs?: number;
   stackEventsPollIntervalMs?: number;
 }
+
+export interface KnownCliOptions {
+  clusterName: string;
+  stackName: string;
+  verbose: boolean;
+  includeFargate: boolean;
+  awsAccessKeyId: string;
+  awsSecretAccessKey: string;
+  awsSessionToken: string;
+  assumeRoleArn: string;
+  externalId: string;
+  awsProfile: string;
+  region: string;
+}
